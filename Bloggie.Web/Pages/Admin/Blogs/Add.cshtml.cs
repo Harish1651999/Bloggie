45bed982312283e8c8954ab6b72b48db691a7823
Harish1651999 <hk1651999@gmail.com>
2025-10-04 18:24:46 +0530

    save to database.

--- Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,8 +8,15 @@ namespace Bloggie.Web.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
         [BindProperty]
         public AddBlogPost AddBlogPostRequest { get; set; }
+
+        public AddModel(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
 
         public void OnGet()
         {
@@ -15,6 +24,21 @@ namespace Bloggie.Web.Pages.Admin.Blogs
 
         public void OnPost()
         {
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPostRequest.Heading,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                Content = AddBlogPostRequest.Content,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                FeaturedImageUrl = AddBlogPostRequest.FeaturedImageUrl,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                Author = AddBlogPostRequest.Author,
+                Visible = AddBlogPostRequest.Visible
+            };
+
+            bloggieDbContext.BlogPosts.Add(blogPost);
+            bloggieDbContext.SaveChanges();
         }
     }
 }
